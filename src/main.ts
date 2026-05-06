a353dc8820ce81e887afd6120ff81d0e1f37905a
John <johnbro1218@gmail.com>
2026-05-06 21:11:05 +0200

    Añadir logos equipos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
 
   app.enableCors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
