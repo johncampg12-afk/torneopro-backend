157c44dd0fffe9ef622c617fd48c403718017825
John <johnbro1218@gmail.com>
2026-05-06 20:20:35 +0200

    Escudos Equipos

--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -14,6 +14,7 @@ export class TeamsService {
       data: {
         name: dto.name,
         color: dto.color || '#3b82f6',
+        logo: dto.logo,
         tournamentId,
       },
       include: { players: true },
